777c173928f96f8302d282e0e3d7d89d29fa95fe
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-06-07 00:26:18 +0530

    create database

---
+++ src/db/adoptMe.js
@@ -0,0 +1,14 @@
+import GoldenRetriever from '../assets/golden_retriever.jpg';
+import LabradorRetriever from '../assets/labrador_retriever.jpg';
+import SiberianHusky from '../assets/siberian_husky.jpg';
+import Bulldog from '../assets/bulldog.jpg';
+import Beagle from '../assets/beagle.jpg';
+import GermanShepherd from '../assets/german_shepherd.jpg';
+
+
+export const dogs = [ { "id":1 , "pic":<img src={GoldenRetriever} alt='adopt-me-Golden-Retriever' />, "name":"Bella","breed":"Golden Retriever","price":1200},
+{ "id":2 , "pic":<img src={LabradorRetriever} alt="adopt-me-Labrador-Retriever"/>, "name":"Max","breed":"Labrador Retriever","price":900},
+{ "id":3 , "pic":<img src={SiberianHusky} alt="adopt-me-siberian-husky"/>, "name":"Luna","breed":"Siberian Husky","price":1500},
+{ "id":4 , "pic":<img src={Bulldog} alt="adopt-me-bulldog" />, "name":"Charlie","breed":"French Bulldog","price":2500},
+{ "id":5 , "pic":<img src={Beagle} alt="adopt-me-beagle" />, "name":"Daisy","breed":"Beagle","price":800},
+{ "id":6 , "pic":<img src={GermanShepherd} alt="adopt-me0german-shepherd" />, "name":"Rocky","breed":"German Shepherd","price":1800}];
